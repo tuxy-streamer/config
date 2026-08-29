f702ca098e317f9bc0dd2f6f6e4216c2dc687595
tuxy <subham020304@gmail.com>
2026-08-29 06:04:05 +0530

    fix(shell): add temperature value

--- shell/scripts/nightlight.sh
+++ shell/scripts/nightlight.sh
@@ -2,5 +2,5 @@
 
 nightlight_toggle(){
     { [ $(pgrep -x "hyprsunset") ] && killall -KILL hyprsunset && notify-send "Night Light" "OFF" --urgency=low; } ||
-        { hyprsunset & notify-send "Night Light" "ON" --urgency=low; }
+        { hyprsunset -t 5000 & notify-send "Night Light" "ON" --urgency=low; }
 }
