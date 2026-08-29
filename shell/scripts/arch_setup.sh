81607a718f62aa25c83aaebbf2b33171720cbd00
tuxy <subham020304@gmail.com>
2026-08-29 06:04:56 +0530

    feat(shell): add more software categories

--- shell/scripts/arch_setup.sh
+++ shell/scripts/arch_setup.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 logdir="$HOME/log/"
-mkdir $logdir
-js="nodejs npm pnpm bun prettier"
-go="go"
-rust="rustup cargo"
+[ -d "$logdir" ] || mkdir "$logdir"
+lang_js="nodejs npm pnpm bun prettier"
+lang_go="go"
+lang_rust="rustup cargo"
+lang_python="uv pyenv"
 db="postgres mongodb"
-env="fzf ripgrep fd neovim rofi dunst qtile hyprland"
-terminal="xterm foot"
+terminal="xterm foot foot-terminfo"
+pack_zathura="zathura zathura-pdf-mupdf tesseract-data-eng"
+pack_hypr="hyprland uwsm hypridle hyprlock hyprsunset hyprshot awww satty"
+pack_retroarch="retroarch retroarch-assets-glui retroarch-assets-ozone retroarch-assets-xmb libretro-overlays libretro-shaders gamemode"
+pack_doc="wikiman arch-wiki-docs tealdeer"
+env="$pack_hypr ripgrep fd neovim github-cli lazygit rofi tmux dunst qtile ly"
+system_util="$pack_zathura bat fzf starship zoxide networkmanager btop sc-im tealdeer bleachbit preload dua"
+emulator="$pack_retroarch eden emulationstation-de"
 
 check_permission() {
 	[ "$(id -u)" -ne 0 ] && echo "This script must be run as root." && return
@@ -33,4 +40,9 @@ aur_setup() {
 	sudo $noconfirm_pacman -Syu | tee -a "$logdir/$logfile"
 	command -v paru >/dev/null 2>&1 && printf 'Success: Found ' | tee -a "$logdir/$logfile" ||
 		sudo $noconfirm_pacman -S paru | tee -a "$logdir/$logfile"
+}
+
+zmk_setup() {
+	uv tool install zmk
+	sudo noconfirm_pacman -Sy zmk-studio-bin
 }
